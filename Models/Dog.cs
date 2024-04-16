d6ac2df951b4f77e106c17a4578e00c758b82493
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-04-16 10:44:29 -0500

    Made WalkerId property nullable

--- Models/Dog.cs
+++ Models/Dog.cs
@@ -4,6 +4,6 @@ public class Dog
 {
     public int Id {get; set;}
     public string Name {get; set;} = string.Empty;
-    public int WalkerId {get; set;}
+    public int? WalkerId {get; set;}
     public int CityId {get; set;}
 }
