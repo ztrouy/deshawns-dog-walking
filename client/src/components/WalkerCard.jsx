47acf3c9719d7e78725362de733c1c63f0a24e5b
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-04-17 17:39:28 -0500

    Wrote rendering for a Walker card

---
+++ client/src/components/WalkerCard.jsx
@@ -0,0 +1,17 @@
+import { Card, CardBody, CardText, Button } from "reactstrap"
+
+export const WalkerCard = ({ walker }) => {
+    return (
+        <Card className="mb-2">
+            <CardBody className="row gx-1">
+                <div className="col-8 d-flex">
+                    <CardText className="fs-4 fw-bold" role="button">{walker.name}</CardText>
+                </div>
+                <div className="col-4 d-flex gap-2 justify-content-end">
+                    <Button>Remove</Button>
+                    <Button>Add Dog</Button>
+                </div>
+            </CardBody>
+        </Card>
+    )
+}
