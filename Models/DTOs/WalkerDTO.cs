778c7e6e08b685ca5355c53c2051b4afb40062b9
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-04-17 16:32:56 -0500

    Added list properties for CityWalkerDTO, CityDTO, and DogDTO

--- Models/DTOs/WalkerDTO.cs
+++ Models/DTOs/WalkerDTO.cs
@@ -4,4 +4,7 @@ public class WalkerDTO
 {
     public int Id {get; set;}
     public string Name {get; set;} = string.Empty;
+    public List<CityWalkerDTO> CityWalkers {get; set;}
+    public List<CityDTO> Cities {get; set;}
+    public List<DogDTO> Dogs {get; set;}
 }
