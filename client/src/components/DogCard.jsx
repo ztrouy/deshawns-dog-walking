e3f174f42cfcf1ee9fc70ebe1a70a9863f0e918c
Zachary Trouy <ztrouy@gmail.com>
2024-04-17 11:03:43 -0500

    Added logic to call Dog Details modal when Dog name is clicked

--- client/src/components/DogCard.jsx
+++ client/src/components/DogCard.jsx
@@ -1,11 +1,24 @@
 import { Card, CardBody, CardText, Button } from "reactstrap"
+import { getDogById } from "../apiManager"
 
-export const DogCard = ({ dog }) => {
+export const DogCard = ({ dog, setChosenDog, toggleModal }) => {
+    const handleClick = () => {
+        getDogById(dog.id).then(dogObject => {
+            setChosenDog(dogObject)
+        })
+        toggleModal()
+    }
+    
     return (
         <Card className="mb-2">
             <CardBody className="row gx-1">
                 <div className="col-8 d-flex">
-                    <CardText className="fs-4 fw-bold">{dog.name}</CardText>
+                    <CardText 
+                        className="fs-4 fw-bold" 
+                        onClick={handleClick}
+                        // data-bs-toggle={"modal"}
+                        // data-bs-target={"#dogDetailsModal"}
+                    >{dog.name}</CardText>
                 </div>
                 <div className="col-4 d-flex justify-content-end">
                     <Button>Remove</Button>
