86a839ac9158413c837515b8b3a8991241400eca
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-04-18 14:08:56 -0500

    Updated rendering and logic to handle being used in new contexts

--- client/src/components/DogCard.jsx
+++ client/src/components/DogCard.jsx
@@ -1,22 +1,42 @@
 import { Card, CardBody, CardText, Button } from "reactstrap"
-import { getDogById } from "../apiManager"
+import { assignDogWalker, getDogById } from "../apiManager"
 
-export const DogCard = ({ dog, setChosenDog, toggleModal }) => {
-    const handleClick = () => {
+export const DogCard = ({ dog, setChosenDog, toggleModal, toggleOtherModal, showRemove, showAdd, isNameClickable, newWalker }) => {
+    const handleNameClick = () => {
         getDogById(dog.id).then(dogObject => {
             setChosenDog(dogObject)
         })
         toggleModal()
     }
+
+    const handleAssign = () => {
+        const assignObject = {
+            id: dog.id,
+            cityId: dog.cityId,
+            walkerId: newWalker.id
+        }
+
+        assignDogWalker(assignObject).then(dogObject => {
+            setChosenDog(dogObject)
+            toggleOtherModal()
+            toggleModal()
+        })
+    }
     
+
     return (
         <Card className="mb-2">
             <CardBody className="row gx-1">
                 <div className="col-8 d-flex">
-                    <CardText className="fs-4 fw-bold" role="button" onClick={handleClick}>{dog.name}</CardText>
+                    {isNameClickable ? (
+                        <CardText className="fs-4 fw-bold" role="button" onClick={handleNameClick}>{dog.name}</CardText>
+                    ) : (
+                        <CardText className="fs-4 fw-bold">{dog.name}</CardText>
+                    )}
                 </div>
                 <div className="col-4 d-flex justify-content-end">
-                    <Button>Remove</Button>
+                    {showRemove && <Button>Remove</Button>}
+                    {showAdd && <Button onClick={handleAssign}>Assign</Button>}
                 </div>
             </CardBody>
         </Card>
