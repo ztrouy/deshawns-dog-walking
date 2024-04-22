f0d5ed54ef206cfd91d8cf115e866ede3a1b70cd
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-04-21 19:49:55 -0500

    Added logic for clicking on a Walker's name

--- client/src/components/WalkerCard.jsx
+++ client/src/components/WalkerCard.jsx
@@ -1,16 +1,21 @@
 import { Card, CardBody, CardText, Button } from "reactstrap"
 
-export const WalkerCard = ({ walker, setChosenWalker, toggleModal }) => {
+export const WalkerCard = ({ walker, setChosenWalker, toggleModal, toggleWalkerDetailsModal }) => {
     const handleAddDog = () => {
         setChosenWalker(walker)
         toggleModal()
+    }
+
+    const handleNameClick = () => {
+        setChosenWalker(walker)
+        toggleWalkerDetailsModal()
     }
 
     return (
         <Card className="mb-2">
             <CardBody className="row gx-1">
                 <div className="col-8 d-flex">
-                    <CardText className="fs-4 fw-bold" role="button">{walker.name}</CardText>
+                    <CardText className="fs-4 fw-bold" role="button" onClick={handleNameClick}>{walker.name}</CardText>
                 </div>
                 <div className="col-4 d-flex gap-2 justify-content-end">
                     <Button>Remove</Button>
